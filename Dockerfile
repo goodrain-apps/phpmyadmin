96ec5a3910b51a49df69570ef559132778c8a7bf
zhouyq <zhouyq@goodrain.com>
2016-08-16 00:10:22 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM alpine:latest
+
+RUN apk add --no-cache php5-cli php5-mysqli php5-ctype php5-xml php5-gd php5-zlib php5-bz2 php5-zip php5-openssl php5-curl php5-opcache php5-json
+
+COPY phpmyadmin.keyring /
+
+ENV VERSION 4.6.3
+ENV URL https://files.phpmyadmin.net/phpMyAdmin/${VERSION}/phpMyAdmin-${VERSION}-all-languages.tar.gz
+
+RUN set -x \
+    && export GNUPGHOME="$(mktemp -d)" \
+    && apk add --no-cache curl wget gnupg \
+    && curl --output phpMyAdmin.tar.gz --location $URL \
+    && curl --output phpMyAdmin.tar.gz.asc --location $URL.asc \
+    && gpgv --keyring /phpmyadmin.keyring phpMyAdmin.tar.gz.asc phpMyAdmin.tar.gz \
+    && apk del --no-cache gnupg \
+    && rm -rf "$GNUPGHOME" \
+    && tar xzf phpMyAdmin.tar.gz \
+    && rm -f phpMyAdmin.tar.gz phpMyAdmin.tar.gz.asc \
+    && mv phpMyAdmin* /www \
+    && rm -rf /www/js/jquery/src/ /www/examples /www/po/
+
+COPY config.inc.php /www/
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod u+rwx /docker-entrypoint.sh
+
+
+EXPOSE 80
+
+ENV PHP_UPLOAD_MAX_FILESIZE=64M \
+    PHP_MAX_INPUT_VARS=2000
+
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
---
+++ config.inc.php
@@ -0,0 +1,53 @@
+<?php
+require('./config.secret.inc.php');
+/* Ensure we got the environment */
+$vars = array(
+    'PMA_ARBITRARY',
+    'MYSQL_HOST',
+    'MYSQL_HOSTS',
+    'MYSQL_PORT',
+    'MYSQL_USER',
+    'MYSQL_PASS',
+);
+foreach ($vars as $var) {
+    if (!isset($_ENV[$var]) && getenv($var)) {
+        $_ENV[$var] = getenv($var);
+    }
+}
+/* Arbitrary server connection */
+if (isset($_ENV['PMA_ARBITRARY']) && $_ENV['PMA_ARBITRARY'] === '1') {
+    $cfg['AllowArbitraryServer'] = true;
+}
+/* Figure out hosts */
+/* Fallback to default linked */
+$hosts = array('db');
+/* Set by environment */
+if (!empty($_ENV['MYSQL_HOST'])) {
+    $hosts = array($_ENV['MYSQL_HOST']);
+} elseif (!empty($_ENV['MYSQL_HOSTS'])) {
+    $hosts = explode(',', $_ENV['MYSQL_HOSTS']);
+}
+/* Server settings */
+for ($i = 1; isset($hosts[$i - 1]); $i++) {
+    $cfg['Servers'][$i]['host'] = $hosts[$i - 1];
+    if (isset($_ENV['MYSQL_PORT'])) {
+        $cfg['Servers'][$i]['port'] = $_ENV['MYSQL_PORT'];
+    }
+    if (isset($_ENV['MYSQL_USER'])) {
+        $cfg['Servers'][$i]['auth_type'] = 'config';
+        $cfg['Servers'][$i]['user'] = $_ENV['MYSQL_USER'];
+        $cfg['Servers'][$i]['password'] = isset($_ENV['MYSQL_PASS']) ? $_ENV['MYSQL_PASS'] : null;
+    } else {
+        $cfg['Servers'][$i]['auth_type'] = 'cookie';
+    }
+    $cfg['Servers'][$i]['connect_type'] = 'tcp';
+    $cfg['Servers'][$i]['compress'] = false;
+    $cfg['Servers'][$i]['AllowNoPassword'] = true;
+}
+/* Uploads setup */
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+/* Include User Defined Settings Hook */
+if (file_exists('/config.user.inc.php')) {
+    include('/config.user.inc.php');
+}
