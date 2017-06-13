1c37fb352e5ef7b601e4f2ee0d46fc22dfb2ab1b
zhouyq <zhouyq@goodrain.com>
2017-06-13 18:34:29 +0800

    change phpmyadmin config

--- config.inc.php
+++ config.inc.php
@@ -1,53 +1,9 @@
 <?php
-require('./config.secret.inc.php');
-/* Ensure we got the environment */
-$vars = array(
-    'PMA_ARBITRARY',
-    'MYSQL_HOST',
-    'MYSQL_HOSTS',
-    'MYSQL_PORT',
-    'MYSQL_USER',
-    'MYSQL_PASS',
-);
-foreach ($vars as $var) {
-    if (!isset($_ENV[$var]) && getenv($var)) {
-        $_ENV[$var] = getenv($var);
-    }
-}
-/* Arbitrary server connection */
-if (isset($_ENV['PMA_ARBITRARY']) && $_ENV['PMA_ARBITRARY'] === '1') {
-    $cfg['AllowArbitraryServer'] = true;
-}
-/* Figure out hosts */
-/* Fallback to default linked */
-$hosts = array('db');
-/* Set by environment */
-if (!empty($_ENV['MYSQL_HOST'])) {
-    $hosts = array($_ENV['MYSQL_HOST']);
-} elseif (!empty($_ENV['MYSQL_HOSTS'])) {
-    $hosts = explode(',', $_ENV['MYSQL_HOSTS']);
-}
-/* Server settings */
-for ($i = 1; isset($hosts[$i - 1]); $i++) {
-    $cfg['Servers'][$i]['host'] = $hosts[$i - 1];
-    if (isset($_ENV['MYSQL_PORT'])) {
-        $cfg['Servers'][$i]['port'] = $_ENV['MYSQL_PORT'];
-    }
-    if (isset($_ENV['MYSQL_USER'])) {
-        $cfg['Servers'][$i]['auth_type'] = 'cookie';
-        $cfg['Servers'][$i]['user'] = $_ENV['MYSQL_USER'];
-        $cfg['Servers'][$i]['password'] = isset($_ENV['MYSQL_PASS']) ? $_ENV['MYSQL_PASS'] : null;
-    } else {
-        $cfg['Servers'][$i]['auth_type'] = 'cookie';
-    }
-    $cfg['Servers'][$i]['connect_type'] = 'tcp';
-    $cfg['Servers'][$i]['compress'] = false;
-    $cfg['Servers'][$i]['AllowNoPassword'] = false;
-}
-/* Uploads setup */
-$cfg['UploadDir'] = '';
-$cfg['SaveDir'] = '';
-/* Include User Defined Settings Hook */
-if (file_exists('/config.user.inc.php')) {
-    include('/config.user.inc.php');
-}
+require('/app/phpmyadmin/config.secret.inc.php');
+
+
+$cfg['Servers'][1]['auth_type'] = 'cookie';
+$cfg['Servers'][1]['host']      = _MYSQL_HOST_;
+$cfg['Servers'][1]['port']      = _MYSQL_PORT_;
+$cfg['Servers'][1]['extension'] = 'mysqli';
+?>
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,56 @@
 
 [ $DEBUG ]  && set -x
 
-if [ ! -f /app/phpmyadmin/config.secret.inc.php ] ; then
-    cat > /app/phpmyadmin/config.secret.inc.php <<EOT
+SECRET_CFG="/app/phpmyadmin/config.secret.inc.php"
+PAM_CFG="/app/phpmyadmin/config.inc.php"
+PHP_CFG="/etc/php5/php.ini"
+NGINX_CFG="/etc/nginx/nginx.conf"
+DELIMITER="="
+# php.ini config
+export CFG_UPLOAD_MAX_FILESIZE=${CFG_UPLOAD_MAX_FILESIZE:-128M}
+export CFG_POST_MAX_SIZE=${CFG_POST_MAX_SIZE:-128M}
+
+
+function set_config() {
+    CONFIG_FILE=$1
+    CFG=($( env | sed -nr "s/CFG_([0-9A-Z_a-z-]*)/\1/p"|tr A-Z a-z))
+
+    for CFG_KEY in "${CFG[@]}"; do
+        KEY=`echo $CFG_KEY | cut -d = -f 1`
+        VAR=`echo $CFG_KEY | cut -d = -f 2`
+        if [ "$VAR" == "" ]; then
+            echo "Empty volue for option \"$KEY\"."
+            continue
+        fi
+        grep -q "$KEY" $CONFIG_FILE
+        if (($? > 0)); then
+            echo "${KEY}${DELIMITER}${VAR}" >> $CONFIG_FILE
+            echo "Config add option for \"$KEY\"."
+        else
+            sed -i -r "s~#?($KEY)[ ]*${DELIMITER}.*~\1 ${DELIMITER} $VAR~g" $CONFIG_FILE  >/dev/null 2>&1
+            echo "Option found for \"$KEY\"."
+        fi
+    done
+}
+
+echo "Check and set php config..."
+set_config $PHP_CFG
+
+echo "Generate secret key..."
+if [ ! -f ${SECRET_CFG} ] ; then
+    cat > ${SECRET_CFG} <<EOT
 <?php
 \$cfg['blowfish_secret'] = '`cat /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' | fold -w 32 | head -n 1`';
 EOT
 fi
+
+# replace config with env
+MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+MYSQL_PORT=${MYSQL_PORT:-3306}
+sed -i -r "s/_MYSQL_HOST_/\'$MYSQL_HOST\'/" $PAM_CFG
+sed -i -r "s/_MYSQL_PORT_/\'$MYSQL_PORT\'/" $PAM_CFG
+
+
 
 echo -e "Start php-fpm in background."
 php-fpm5
